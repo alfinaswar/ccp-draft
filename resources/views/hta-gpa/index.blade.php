a10812bdadb68345b28dc5cb973e94c79a686825
Alfin Aswar <alfin7aswar@gmail.com>
2026-08-03 18:19:29 +0700

    A

--- resources/views/hta-gpa/index.blade.php
+++ resources/views/hta-gpa/index.blade.php
@@ -434,28 +434,30 @@
                             <!-- Ajukan Button trigger modal -->
                             @php
                                 $showAjukan = false;
-                                $hasApprovalToken = false;
-                                foreach ($data->getVendor as $idx => $v) {
-                                    // Cek ApprovalToken di getHtaGpa jika ada
-                                    if (!empty($v->getHtaGpa->ApprovalToken ?? null)) {
-                                        $hasApprovalToken = true;
-                                        break;
+                                $filledFileCount = 0;
+                                foreach ($data->getHtaGpa->getDetailHta ?? [] as $detail) {
+                                    if (!empty($detail->File)) {
+                                        $filledFileCount++;
                                     }
                                 }
-                                if (!$hasApprovalToken) {
-                                    foreach ($data->getVendor as $idx => $v) {
-                                        if (!is_null($v->getHtaGpa->Nilai2 ?? null)) {
-                                            $showAjukan = true;
-                                            break;
+                                if ($filledFileCount >= 2) {
+                                    // Cek kalau approval urutan 1 tokennya sudah ada, maka jangan tampilkan tombol
+                                    $approvalUrutan1 = null;
+                                    if (!empty($approval)) {
+                                        foreach ($approval as $item) {
+                                            if (($item->Urutan ?? null) == 1) {
+                                                $approvalUrutan1 = $item;
+                                                break;
+                                            }
                                         }
+                                    }
+                                    if (empty($approvalUrutan1) || empty($approvalUrutan1->ApprovalToken)) {
+                                        $showAjukan = true;
                                     }
                                 }
                             @endphp
-                            @if (
-                                $showAjukan &&
-                                    !$hasApprovalToken &&
-                                    ($data->Status ?? '') !== 'Ditolak' &&
-                                    ($data->getHtaGpa->Status ?? '') !== 'Final')
+
+                            @if ($showAjukan)
                                 <button type="button" id="btnAjukan" class="btn btn-success me-2"
                                     data-bs-toggle="modal" data-bs-target="#modalPenilai">
                                     <i class="fa fa-paper-plane me-1"></i> Ajukan & Kirim Email
