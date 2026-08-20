ead1de459e5118fb1d9006a6c5ff67915af56c3b
Alfinaswar <alfin7aswar@gmail.com>
2026-08-20 08:19:49 +0700

    a

--- resources/views/rekomendasi-pembelian/rekap/hasil-rekomendasi.blade.php
+++ resources/views/rekomendasi-pembelian/rekap/hasil-rekomendasi.blade.php
@@ -163,7 +163,7 @@
             <td>Bmhp</td>
             @foreach ($rekomendasi->getRekomedasiDetail as $item)
                 <td>
-                    {{ $item->Bmhp ?? '-' }}
+                     {!! $item->Bmhp ?? '-' !!}
                 </td>
             @endforeach
         </tr>
